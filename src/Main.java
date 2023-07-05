fade8f70fcf5fdcd12fbfc32b6e8bb11d49a3f4b
capybara <sakibara.ex.9175@gmail.com>
2023-07-05 12:13:02 +0900

    第4回課題　サンプルから

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
-// Shift を 2 回押して 'どこでも検索' ダイアログを開き、`show whitespaces` と入力して
-// Enter キーを押します。これでコードに空白文字が表示されます。
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        // ハイライトされたテキストにキャレットがある状態で Opt+Enter を押して
-        // IntelliJ IDEA が提案する修正方法を表示します。
-        System.out.printf("Hello and welcome!");
 
-        // コードを実行するには Ctrl+R を押すか、ガターにある緑の矢印ボタンをクリックします。
-        for (int i = 1; i <= 5; i++) {
+        List<String> names = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida");
+        List<String> result = new ArrayList<>();
 
-            // Ctrl+D を押してコードのデバッグを開始します。ブレークポイントを 1 つ設定しましたが、
-            // Cmd+F8 を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
+        for (String name : names) {
+            if (name.contains("a")) {
+                result.add(name);
+            }
         }
+
     }
 }
