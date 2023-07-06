f0bd16d28426741488b86b5144a77b668af05b26
capybara <sakibara.ex.9175@gmail.com>
2023-07-06 11:53:41 +0900

    第4回課題

--- src/MethodReference.java
+++ src/MethodReference.java
@@ -6,28 +6,15 @@ public class MethodReference {
     public static void main(String[] args) {
         List<String> names = List.of("Tama", "Hachi", "suzuki", "Goro", "Nana");
 
+        long c1 = names.stream().filter(name -> name.contains("a")).count();
+        System.out.println(c1);
+
         System.out.println("[複数条件で表示したい]");
         names = names.stream()
                 .sorted()
                 .map(String::toUpperCase)
                 .collect(Collectors.toList());
         System.out.println(names);
-
-
-        System.out.println("「リストを昇順に表示」");
-        List<String> result = names.stream().sorted().toList();
-
-        for (String name : result) {
-            System.out.println(name);
-        }
-
-
-        System.out.println("「大文字で表示」");
-        names.stream().map(String::toUpperCase).forEach(System.out::println);
-
-
-        long c1 = names.stream().filter(name -> name.contains("a")).count();
-        System.out.println(c1);
 
 
     }
