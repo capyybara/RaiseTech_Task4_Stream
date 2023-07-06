afc71d4c5441541b3613ba35ae83d28f10671bcb
capybara <sakibara.ex.9175@gmail.com>
2023-07-06 12:01:30 +0900

    第4回課題

--- src/StreamPractice.java
+++ src/StreamPractice.java
@@ -1,17 +1,24 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamPractice {
 
     public static void main(String[] args) {
         List<String> names = List.of("Tama", "Hachi", "suzuki", "Goro", "Nana");
-        List<String> sortedResult = names.stream().sorted().toList();
-
-        for (String name : sortedResult) {
-            System.out.println(name);
-        }
 
         long c1 = names.stream().filter(name -> name.contains("a")).count();
         System.out.println(c1);
+
+        System.out.println("[複数条件で表示したい]");
+        names = names.stream()
+                .sorted()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        for (String catname : names){
+            System.out.println(catname);
+        }
+
 
     }
 }
